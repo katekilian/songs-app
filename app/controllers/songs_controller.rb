c9282db3d3f358628d527a70429699227e16e311
Kate Kilian Vovk <kathrynjkilian@gmail.com>
2015-02-16 15:03:31 -0700

    Refactored New and Edit views to include _form partial, added destroy action to controller

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -12,10 +12,37 @@ class SongsController < ApplicationController
     @song = Song.new
   end
 
+  def create
+    @song = Song.new(song_params)
+    if @song.save
+      redirect_to songs_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @song = Song.find(params[:id])
+  end
+
+  def update
+    @song = Song.find(params[:id])
+    if @song.update(song_params)
+      redirect_to song_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @song = Song.destroy(params[:id])
+    redirect_to songs_path
+  end
+
   private
 
   def song_params
-    params.require(:songs).permit(:title, :songwriter, :performer)
+    params.require(:song).permit(:title, :songwriter, :performer)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
 
-  resources :songs, only: [:index, :show, :new]
+  resources :songs, only: [:index, :show, :new, :create, :edit, :update, :destroy]
 
 end
